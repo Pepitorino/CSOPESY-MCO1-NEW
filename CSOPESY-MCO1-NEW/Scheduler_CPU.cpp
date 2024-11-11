a8ecc35c9a16de5182aa2fa45308943c22b6b2ac
Pepitorino <carlos.ambrosio2007@gmail.com>
2024-11-11 22:30:03 +0800

    added LV CPUSerf changes from commit a23682e

--- CSOPESY-MCO1-NEW/Scheduler_CPU.cpp
+++ CSOPESY-MCO1-NEW/Scheduler_CPU.cpp
@@ -41,7 +41,7 @@ bool Scheduler::CPUProcessRequest(int CPUid) {
 
 		//check if processQueue is empty
 		if (processQueue.empty()) {
-			if (SCHEDULER_FOR_THE_STREETS->cpuList.at(CPUid) != nullptr) SCHEDULER_FOR_THE_STREETS->cpuList.at(CPUid)->switchProcess(Process_ToGive);
+			SCHEDULER_FOR_THE_STREETS->cpuList.at(CPUid)->switchProcess(Process_ToGive);
 			return false;
 		}
 		//if not empty, get the front process
@@ -50,6 +50,8 @@ bool Scheduler::CPUProcessRequest(int CPUid) {
 		SCHEDULER_FOR_THE_STREETS->cpuList.at(CPUid)->switchProcess(Process_ToGive);
 		//pop the process from the processQueue
 		processQueue.pop();
+		//give the process to the CPUSerf
+		SCHEDULER_FOR_THE_STREETS->cpuList.at(CPUid)->switchProcess(Process_ToGive);
 		return true;
 		//auto unlock mutex when lock_guard goes out of scope
 	}
